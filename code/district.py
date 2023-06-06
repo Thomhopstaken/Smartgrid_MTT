fb03565ee6a8eb82bc130cd5e9e49c8fbd3e0e8e
thmichaellin <thmichaellin@gmail.com>
2023-06-06 10:58:55 +0200

    TEST
    asrfasd

--- code/district.py
+++ code/district.py
@@ -1,59 +1,25 @@
-from batterijen import Batterijen
-from huizen import Huizen
-from typing import TextIO
-import os
+# import huizen.py 
+# import batterijen.py
 
 
-class District:
+#class district
 
-    def __init__(self, district) -> None:
-        
-        self.districtnummer = district
-        self.batterijen = []
-        self.huizen = [] 
-        
-        self.laad_batterijen(f"{self.bestand_vinden('batteries')}")
-        self.laad_huizen(f"{self.bestand_vinden('houses')}")
+# def init
+    # neemt districtnaam als input. 
     
-    def laad_batterijen(self, bestand):
-        """Neemt data van bestand en maakt daarmee batterijobjecten.
-        deze worden ongebracht in batterijlijst."""
-        
+    # var district
+    # var batterijen (lijst)
+    # var huizen (lijst)
+    
+# def laad_batterijen.
     # neemt file eindigend op batteries.csv als input. 
-        with open(bestand, 'r') as b:
-            teller = len(b.readlines())
-        with open(bestand, 'r') as b:
-            for x in range(0, teller):
-                data = self.data_inladen(b)
-        # voeg batterij object aan batterij-lijst toe.
-                if data[0].isnumeric():
-                    self.batterijen.append(Batterijen(int(data[0]), int(data[1]), float(data[2])))
+    # itereer over de file lijnen en maak batterij class aan 
+    # voeg batterij object aan batterij-lijst toe.
 
-    def laad_huizen(self, bestand):
-        """Neemt data van bestand en maakt daarna huisobjecten.
-        deze worden ondergebracht in huislijst."""    
-        
-        # neemt file eindigend op houses.csv als input. 
-        with open(bestand, 'r') as b:
-            teller = len(b.readlines())
-        with open(bestand, 'r') as b:
-            for x in range(0, teller):
-                data = self.data_inladen(b)
-        # voeg batterij object aan huizen-lijst toe.
-                if data[0].isnumeric():
-                    self.huizen.append(Huizen(int(data[0]), int(data[1]), float(data[2])))
 
-    def data_inladen(self, b: TextIO) -> list[str]:
-        """Neemt bestandlijn en maakt er een lijst van."""
-        line = b.readline()
-        line = line.replace("\n", '')
-        line = line.replace('\"','')
-        return line.split(",")
-    
-    def bestand_vinden(self, naam):
-        """Zoekt juiste bestandpad voor opgevraagde naam."""
-        hoofdfolder = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-        district_map = os.path.join(hoofdfolder, 'Huizen&Batterijen', f'district_{self.districtnummer}')
-        bestandnaam = f'district-{self.districtnummer}_{naam}.csv'
-        return os.path.join(district_map, bestandnaam)
-    
+# def laad_huizen
+    # neemt file eindigend op houses.csv als input. 
+    # itereer over de file lijnen en maak huizen class aan 
+    # voeg batterij object aan huizen-lijst toe.
+
+    asjdalkkmdad
--- code/huizen.py
+++ code/huizen.py
@@ -1,8 +1,9 @@
-class Huizen:
 
-    def __init__(self, x, y, maxoutput) -> None:
-        """Neemt gegevens van Huizen uit district en slaat ze op in een Huizen object."""
+#class huizen.
 
-        self.x_as = x
-        self.y_as = y
-        self.maxoutput = maxoutput
+#def init
+    # neemt drie variabelen(x-as, y-as, maxoutput)
+    
+    # var x-as (integer)
+    # var y-as (integer)
+    # var maxoutput (float)
