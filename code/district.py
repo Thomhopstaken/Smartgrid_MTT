fbd5d18f4ee93fb7a65aca682732eef149f62185
Thom Hopstaken <thom.hopstaken@gmail.com>
2023-06-06 09:39:25 +0200

    aanpassingen

--- code/district.py
+++ code/district.py
@@ -1,5 +1,6 @@
 from batterijen import Batterijen
 from huizen import Huizen
+from typing import TextIO
 
 
 class District:
@@ -10,15 +11,35 @@ class District:
         self.batterijen = []
         self.huizen = [] 
         
+        #self.laad_batterijen(f"Smartgrid_MTT/data/Huizen&Batterijen/district_{self.districtnummer}/district-{self.districtnummer}_batteries.csv")
+        self.laad_huizen(f"data/Huizen&Batterijen/district_{self.districtnummer}/district-{self.districtnummer}_houses.csv")
     
     def laad_batterijen(self, bestand):
     # neemt file eindigend op batteries.csv als input. 
-    # itereer over de file lijnen en maak batterij class aan 
-    # voeg batterij object aan batterij-lijst toe.
-        pass
+        with open(bestand, 'r') as b:
+            teller = len(b.readlines())
+        with open(bestand, 'r') as b:
+            for x in range(1, teller):
+                data = self.data_inladen(b)
+        # voeg batterij object aan batterij-lijst toe.
+                self.batterijen.append(Batterijen(data[0],data[1],data[2]))
 
     def laad_huizen(self, bestand):
     # neemt file eindigend op houses.csv als input. 
-    # itereer over de file lijnen en maak huizen class aan 
-    # voeg batterij object aan huizen-lijst toe.
-        pass
+        with open(bestand, 'r') as b:
+            teller = len(b.readlines())
+        with open(bestand, 'r') as b:
+            for x in range(1, teller):
+                data = self.data_inladen(b)
+        # voeg batterij object aan huizen-lijst toe.
+                self.batterijen.append(Huizen(data[0],data[1],data[2]))
+
+    def data_inladen(self, b: TextIO) -> list[str]:
+        """take fileline and load it as list.
+
+        Post: list of strings is returned"""
+
+        line = b.readline()
+        line = line.replace("\n", '')
+        return line.split("\t")
+    
--- code/smartgrid.py
+++ code/smartgrid.py
@@ -1,10 +1,24 @@
-# import batterijen.py 
-# import huizen.py
-# import district.py
+from batterijen import Batterijen
+from district import District
+from huizen import Huizen
 
-# maak een district aan. 
+class Smartgrid:
+    pass
 
-# draai op het nieuwe district de laad_batterijen def. 
 
-# draai op het nieuwe district de laad_huizen def. 
+if __name__ == "__main__":
+    
+    # vraag om district.
+    wijknummer = input('Wijk 1, 2 of 3: ')
+    
+    # maak een district aan. 
+    wijk = District(wijknummer)
+    print(wijk.huizen)
+    # haal bestanden op.
+    
+    
+    
+    #draai op het nieuwe district de laad_batterijen def. 
+
+    #draai op het nieuwe district de laad_huizen def. 
 
