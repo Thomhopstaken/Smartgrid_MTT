79967411fef0006693c8338ba76bf673ebe911bd
Thom Hopstaken <thom.hopstaken@gmail.com>
2023-06-06 11:20:52 +0200

    hotfix

--- code/main.py
+++ code/main.py
@@ -1,10 +1,23 @@
-# import batterijen.py 
-# import huizen.py
-# import district.py
+from batterijen import Batterijen
+from district import District
+from huizen import Huizen
 
-# maak een district aan. 
+class Smartgrid:
+    pass
 
-# draai op het nieuwe district de laad_batterijen def. 
 
-# draai op het nieuwe district de laad_huizen def. 
+if __name__ == "__main__":
+    
+    # vraag om district.
+    wijknummer = input('Wijk 1, 2 of 3: ')
+    
+    # maak een district aan. 
+    wijk = District(wijknummer)
 
+    print('huizen')
+    for huis in wijk.huizen:
+        print(huis.x_as, huis.y_as, huis.maxoutput)
+    
+    print('Batterijen')
+    for batterij in wijk.batterijen:
+        print(batterij.x_as, batterij.y_as, batterij.capaciteit)
