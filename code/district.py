ebc66a607702ff9388870626cf767bfb5a8fbe8c
Thom Hopstaken <thom.hopstaken@gmail.com>
2023-06-06 09:04:30 +0200

    batterij en huis class gemaakt. start aan district

--- code/district.py
+++ code/district.py
@@ -1,24 +1,24 @@
-# import huizen.py 
-# import batterijen.py
+from batterijen import Batterijen
+from huizen import Huizen
 
 
-#class district
+class District:
 
-# def init
-    # neemt districtnaam als input. 
+    def __init__(self, district) -> None:
+        
+        self.districtnummer = district
+        self.batterijen = []
+        self.huizen = [] 
+        
     
-    # var district
-    # var batterijen (lijst)
-    # var huizen (lijst)
-    
-# def laad_batterijen.
+    def laad_batterijen(self, bestand):
     # neemt file eindigend op batteries.csv als input. 
     # itereer over de file lijnen en maak batterij class aan 
     # voeg batterij object aan batterij-lijst toe.
+        pass
 
-
-# def laad_huizen
+    def laad_huizen(self, bestand):
     # neemt file eindigend op houses.csv als input. 
     # itereer over de file lijnen en maak huizen class aan 
     # voeg batterij object aan huizen-lijst toe.
-    
+        pass
