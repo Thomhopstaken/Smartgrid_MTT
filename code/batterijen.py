fec696fd787e43c14a94a343ef81cdde02ad3a74
Thom Hopstaken <thom.hopstaken@gmail.com>
2023-06-07 10:15:09 +0200

    afstand def toevoegd

--- code/batterijen.py
+++ code/batterijen.py
@@ -8,3 +8,9 @@ class Batterijen:
         self.capaciteit = capaciteit
         self.huidig_gebruik = 0
         self.gelinkte_huizen = []
+        self.afstand_huizen = {} 
+    
+    def afstand_berekenen(self, huizen):
+        for huis in huizen:
+            afstand = abs((huis.x_as + huis.y_as) - (self.x_as + self.y_as))
+            self.afstand_huizen[huis] = afstand
