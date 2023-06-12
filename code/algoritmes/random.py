e193b4658b8c8fa198824059eff4bc97a945d473
Thom Hopstaken <thom.hopstaken@gmail.com>
2023-06-12 13:01:13 +0200

    break aan random algo toegevoegd

--- code/algoritmes/random.py
+++ code/algoritmes/random.py
@@ -4,6 +4,7 @@ def random_alg(wijk) -> None:
     """Willekeurig algoritme om huizen aan te sluiten op batterijen in een wijk."""
 
     counter = 0
+    stop_counter = 0
     while len(wijk.losse_huizen) > 0:
         for batterij in wijk.batterijen:
             for huis in wijk.losse_huizen:
@@ -13,8 +14,12 @@ def random_alg(wijk) -> None:
                     wijk.leg_kabel_route(batterij, huis)
                     print(f"{counter}: yes")
                 else:
+                    if counter == 149 and batterij.resterende_capaciteit < huis.maxoutput:
+                        stop_counter += 1
                     print(f'{counter}: error on house {huis.huis_id}')
                     #print(f'cordinates; {huis.x_as}|{huis.y_as}')
                     #print(f'output: {huis.maxoutput}')
                     print(f'capaciteit batterij: {batterij.batterij_id}: {batterij.resterende_capaciteit}')
-                    
+        if stop_counter ==5:
+            print('run failed!')
+            break
