0f3381b187043de5fb57ab19fd07f452a7601dd1
Thom Hopstaken <thom.hopstaken@gmail.com>
2023-06-12 13:59:50 +0200

    while loop in main toegevoegd

--- main.py
+++ main.py
@@ -8,8 +8,15 @@ if __name__ == "__main__":
     wijknummer = input('Wijk 1, 2 of 3: ')
     colors = ['b', 'y', 'r', 'c', 'm']
     # maak een district aan.
-    wijk = district.District(wijknummer)
-
-    random.random_alg(wijk)
+    
+    
+    counter = 0
+    succesvolle_run = False
+    while succesvolle_run == False:
+        wijk = district.District(wijknummer)
+        succesvolle_run = random.random_alg(wijk)
+        counter += 1
+        print(counter)
+        
 
     smartgrid.visualise(wijknummer, wijk)
