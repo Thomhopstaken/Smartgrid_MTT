ee3848d719338ead48d631185fecceefd205017e
thmichaellin <thmichaellin@gmail.com>
2023-06-13 09:24:55 +0200

    Visualisatie update
    House colors

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ from code.visualisatie import smartgrid
 if __name__ == "__main__":
 
     wijknummer = input('Wijk 1, 2 of 3: ')
-    colors = ['b', 'y', 'r', 'c', 'm']
     # maak een district aan.
     
     
