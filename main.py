f487e51707d3ceaac3cba80be4acfe9251086ded
Thom Hopstaken <thom.hopstaken@gmail.com>
2023-06-13 10:14:22 +0200

    greedy aan ui toegevoegd

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from code.algoritmes import random
+from code.algoritmes import greedy
 from code.klassen import district
 from code.visualisatie import smartgrid
 
+def check_succes():
 
 if __name__ == "__main__":
 
     wijk_kiezen = input('Kies wijk 1, 2 of 3: ')
-    algoritme_kiezen = input('Kies uit algoritme (R)andom: ')
+    algoritme_kiezen = input('Kies uit algoritme (R)andom, (G)reedy: ')
     aantal_runs = int(input('geef aantal runs: '))
     
     run_succesvol = False
@@ -22,11 +24,16 @@ if __name__ == "__main__":
                 succesvolle_runs[wijk] = wijk.prijskaartje
             else: 
                 mislukte_runs += 1
+    elif algoritme_kiezen == 'G' or algoritme_kiezen == "Greedy":
+        wijk = district.District(wijk_kiezen, 1)
+        run = greedy.greedy_alg(wijk)
+        wijk.kosten_berekening()
+        succesvolle_runs = wijk.prijskaartje   
     else: 
         print('Invalid Argument')
     
     if len(succesvolle_runs) > 0:
-        goedkoopste_run = min(succesvolle_runs, key=lambda x: succesvolle_runs[x])
+        goedkoopste_run = min(succesvolle_runs, key= lambda x: succesvolle_runs[x])
         gemiddelde_prijs = int(sum(succesvolle_runs.values()) / len(succesvolle_runs))
         
         print('')
