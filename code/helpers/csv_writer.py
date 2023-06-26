0ab3c4020042ea5b47cb95ec7bd3060dc56950be
Thom Hopstaken <thom.hopstaken@gmail.com>
2023-06-26 11:48:40 +0200

    opschonen kmeans, csv_writer en helpers

--- code/helpers/csv_writer.py
+++ code/helpers/csv_writer.py
@@ -1,33 +1,37 @@
 import csv
+
+
 class Write_csv:
 
-    def __init__(self, bestand):
+    def __init__(self, bestand: str) -> None:
         self.bestand = bestand
 
-    def batterij(self, batterijen, centroids):
+    def batterij(self, batterijen: list[object],
+                 centroids: list[list[float]]) -> None:
         with open(self.bestand, 'w', newline='') as b:
-            writer = csv.writer(b)
+            schrijver = csv.writer(b)
             veld = ["positie", "capaciteit"]
-            writer.writerow(veld)
+            schrijver.writerow(veld)
             for i in range(len(batterijen)):
-                writer.writerow([centroids[i], batterijen[i]])
+                schrijver.writerow([centroids[i], batterijen[i]])
 
-    def huizen(self, huizen):
+    def huizen(self, huizen: list[object]) -> None:
         with open(self.bestand, 'w', newline='') as b:
-            writer = csv.writer(b)
+            schrijver = csv.writer(b)
             veld = ["x", "y", "maxoutput"]
-            writer.writerow(veld)
+            schrijver.writerow(veld)
             for ind in huizen.index:
-                writer.writerow(
-                    [huizen['x'][ind], huizen['y'][ind], huizen['maxoutput'][ind]])
+                schrijver.writerow(
+                    [huizen['x'][ind], huizen['y'][ind],
+                     huizen['maxoutput'][ind]])
 
-    def maak_kosten(self):
+    def maak_kosten(self) -> None:
         with open(self.bestand, 'w', newline='') as b:
             schrijver = csv.writer(b)
             veld = ["kosten"]
             schrijver.writerow(veld)
 
-    def append_kosten(self, kosten):
+    def append_kosten(self, kosten: int) -> None:
         with open(self.bestand, 'a', newline='') as b:
             schrijver = csv.writer(b)
             veld = [kosten]
